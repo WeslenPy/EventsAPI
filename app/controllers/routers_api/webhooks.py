3d84d90b43bedeef64a969f4af5324f61e8c8fef
WeslenPy <weslenjhony@gmail.com>
2022-08-10 08:54:32 -0300

    set logging

--- app/controllers/routers_api/webhooks.py
+++ app/controllers/routers_api/webhooks.py
@@ -1,10 +1,12 @@
 from app import app, db, mp_api
 from flask import jsonify, request
-
+from app.utils.functions import logging
 
 @app.route('/api/v1/webhook/payment', methods=['POST'])
 def webhook_mercadopago():
     data = request.get_json()
+
+    logging.loggingFile('logWebhookPayment',[data])
     
     if "data" in data and "id" in data['data']:
         id_pagamento = data['data']['id']
