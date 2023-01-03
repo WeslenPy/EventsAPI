1e7eb2a363c14ca4577e1310f8bfe08935cb02c1
WeslenPy <weslenjhony@gmail.com>
2023-01-02 21:56:35 -0300

    typing

--- app/__init__.py
+++ app/__init__.py
@@ -12,30 +12,29 @@ from api import MercadoPago,ApiBrasil
 import boto3
 
 
-app = Flask(__name__)
+app:Flask = Flask(__name__)
 app.config.from_pyfile("config.py")
 
-cors = CORS(app)
-db = SQLAlchemy(app)
-ma = Marshmallow(app)
-migrate = Migrate(app,db)
+cors:CORS = CORS(app)
+db:SQLAlchemy = SQLAlchemy(app)
+ma:Marshmallow = Marshmallow(app)
+migrate:Migrate = Migrate(app,db)
 
-mp_api = MercadoPago(app.config['MP_ACCESS_PRIVATE_TOKEN'],app.config['WEBHOOK_URL_CONFIGS'])
-brasil_api = ApiBrasil()
+mp_api:MercadoPago = MercadoPago(app.config['MP_ACCESS_PRIVATE_TOKEN'],app.config['WEBHOOK_URL_CONFIGS'])
+brasil_api:ApiBrasil = ApiBrasil()
 
-mail = Mail(app)
-tokenSafe = URLSafeTimedSerializer(app.config['SECRET_KEY'])
-executor = Executor(app)
+mail:Mail = Mail(app)
+tokenSafe:URLSafeTimedSerializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+executor:Executor = Executor(app)
 
-s3 = boto3.resource('s3')
-client_s3 = boto3.client('s3')
+s3:boto3.resource = boto3.resource('s3')
+client_s3:boto3.client = boto3.client('s3')
 
 jwt:GenerateJWT = GenerateJWT(app.config['SECRET_KEY'])
 
-db.create_all()
-
 from .routers import *
+from .databases import *
 from .blueprints import v1
 
-
+db.create_all()
 app.register_blueprint(blueprint=v1)
