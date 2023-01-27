635962bdd78253becaf97d3ca28f378e2ec695d8
WeslenPy <weslenjhony@gmail.com>
2023-01-27 10:43:13 -0300

    correction erros

--- app/routers/v1/erros/erros.py
+++ app/routers/v1/erros/erros.py
@@ -8,9 +8,9 @@ app =app.app
 @app.errorhandler(404) 
 @app.errorhandler(400) 
 def error_page(error):
-    return jsonify(message='router not found',code=404,error=False),404
+    return jsonify(message='router not found',code=404,error=True),404
 
 @app.errorhandler(500) 
 def erro_server(error):
-    return jsonify(message='Internal server error',code=500,error=False),500
+    return jsonify(message='Internal server error',code=500,error=True),500
 
